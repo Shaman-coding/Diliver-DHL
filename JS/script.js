0634070b3fa0d4a38c22c4112e4f8415891d6917
Kirill <kirya.zinovev.01@bk.ru>
2020-06-17 22:39:04 +0300

    Make click on button

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,29 @@
-function rooms() {
-  
-}
+const block_button = document.querySelector('.block_button'),
+      btnLearn = block_button.querySelector('.btnLearn'),
+      descText = document.querySelector('.descText');
+
+btnLearn.addEventListener('click', (stl) => {
+    stl = prompt("Введите важную информацию!")
+
+    
+    if(stl != null && stl != "" && stl != Number && stl.length >= 20){
+        descText.style.color = '#ffffff';
+        descText.innerHTML = stl;
+        
+    } else {
+        alert("Произошла ошибка, проверьте ваши данные");
+        for(let i = 0; i < 5; i++){
+            stl = prompt("Введите важную информацию!")
+            if(stl == true){
+                descText.style.color = 'red';
+                descText.innerHTML = stl;
+            }else{
+                alert("Произошла ошибка");
+            }
+
+        }
+    }
+});
+
+
+
